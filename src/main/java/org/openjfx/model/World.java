5bff92ed5bc1e178705bf81bc358aa987f5a99d0
manngard <carl.manngard@gmail.com>
2019-09-17 10:28:30 +0200

    Added Tile Class, Tiletype Class, World Class

---
+++ src/main/java/org/openjfx/model/World.java
@@ -0,0 +1,26 @@
+package org.openjfx.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class World {
+    List<List<Tile>> worldGrid;
+    int worldSideLength;
+    //Player player;
+
+    //Initiates 10 rows for the grid matrix, then fills every row with 10 Tile objects
+    public World(){
+        this.worldSideLength = 10;
+
+        worldGrid = new ArrayList<List<Tile>>();
+        for (int i = 0; i<worldSideLength;i++){
+            worldGrid.add(new ArrayList<Tile>());
+        }
+        for (List<Tile> worldrow : worldGrid){
+            for (int i = 0; i<worldSideLength;i++){
+                worldrow.add(new Tile(Tiletype.GRASS));
+            }
+        }
+    }
+
+}
---
+++ src/test/java/org/openjfx/model/WorldTest.java
@@ -0,0 +1,9 @@
+package org.openjfx.model;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WorldTest {
+    
+}
