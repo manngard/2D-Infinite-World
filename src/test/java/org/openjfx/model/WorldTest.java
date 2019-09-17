f08f02e33af41757fb18eecb339752985f101f18
manngard <carl.manngard@gmail.com>
2019-09-17 10:53:44 +0200

    Added tests for World Class

--- src/test/java/org/openjfx/model/WorldTest.java
+++ src/test/java/org/openjfx/model/WorldTest.java
@@ -1,9 +1,26 @@
 package org.openjfx.model;
 
+import org.junit.Before;
+import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class WorldTest {
-    
+    private World world;
+
+    @Before
+    public void createWorld(){
+        this.world = new World();
+    }
+    @Test
+    public void worldConstructorTest(){
+        int columns = 0;
+        for (List<Tile> worldrow: world.worldGrid){
+            assertTrue(worldrow.size() == world.worldSideLength);
+            columns++;
+        }
+        assertTrue(columns == world.worldSideLength);
+    }
+
 }
