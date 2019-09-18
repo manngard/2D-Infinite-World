73e838afc8c0ca8f2100bf34c659f91d3bf5214e
Pautrik <patrikemanuelson@gmail.com>
2019-09-18 14:32:51 +0200

    First step towards MVC

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(new AnchorPane(), 720, 720);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/org/openjfx/controller/Controller.java
@@ -0,0 +1,7 @@
+package org.openjfx.controller;
+
+public class Controller {
+    public void buttonPressed() {
+
+    }
+}
