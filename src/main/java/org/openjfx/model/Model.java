556b7f4d35f026d639a78b5551addf15bff4b081
Pautrik <patrikemanuelson@gmail.com>
2019-09-19 12:03:40 +0200

    Connected key events from view to controller which invokes methods in model

---
+++ src/main/java/org/openjfx/model/Model.java
@@ -0,0 +1,30 @@
+package org.openjfx.model;
+
+public class Model {
+    private World world;
+
+    public Model() {
+        world = new World();
+    }
+
+    public void movePlayerUp() {
+        System.out.println("Up");
+        //TODO
+    }
+
+    public void movePlayerDown() {
+        //TODO
+        System.out.println("Down");
+    }
+
+    public void movePlayerRight() {
+        //TODO
+        System.out.println("Right");
+    }
+
+    public void movePlayerLeft() {
+        //TODO
+        System.out.println("Left");
+    }
+
+}
--- src/main/java/org/openjfx/view/View.java
+++ src/main/java/org/openjfx/view/View.java
@@ -1,16 +1,21 @@
 package org.openjfx.view;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class View {
     private Stage stage;
 
-    public View(Stage stage) {
+    public View(Stage stage, EventHandler<KeyEvent> handler) {
         this.stage = stage;
         Scene scene = new Scene(new AnchorPane(), 700, 700);
         stage.setScene(scene);
         stage.show();
+        stage.addEventFilter(KeyEvent.KEY_PRESSED, handler);
     }
+
+
 }
