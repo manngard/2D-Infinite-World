04d62dc65fa1d2c218df9d8f57130619d05f65b8
Eddy <54838825+EddyIT99@users.noreply.github.com>
2019-09-19 15:08:40 +0200

    Changes and refactoring made to Player and PlayerTest classes

--- src/main/java/org/openjfx/model/Player.java
+++ src/main/java/org/openjfx/model/Player.java
@@ -1,18 +1,16 @@
 package org.openjfx.model;
 
 //  This is the Player class, with variables related to the player . Obs: Should also inherit Interfaces in the future
-public class Player {
+public class Player implements Movable{
 
-    String id;
-    String name;
-    int xcoord, ycoord;
-    boolean isAlive;
-    int hp;
-    int exp;
+    private String id;
+    private int xcoord, ycoord;
+    private boolean isAlive;
+    private int hp;
+    private int exp;
 
-    public Player(String id, String name, int xcoord, int ycoord, boolean isAlive, int hp, int exp) {
+    public Player(String id, int xcoord, int ycoord, boolean isAlive, int hp, int exp) {
         this.id = id;
-        this.name = name;
         this.xcoord = xcoord;
         this.ycoord = ycoord;
         this.isAlive = isAlive;
@@ -20,65 +18,75 @@ public class Player {
         this.exp = exp;
     }
 
-    //  This method will be inherited from an interface "Movable"
-    void move(){
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public int getXcoord() {
         return xcoord;
     }
 
-    public void setXcoord(int xcoord) {
-        this.xcoord = xcoord;
-    }
-
     public int getYcoord() {
         return ycoord;
     }
 
-    public void setYcoord(int ycoord) {
-        this.ycoord = ycoord;
-    }
-
-    public boolean isAlive() {
+    public boolean getisAlive() {
         return isAlive;
-    }
-
-    public void setAlive(boolean alive) {
-        isAlive = alive;
     }
 
     public int getHp() {
         return hp;
     }
 
-    public void setHp(int hp) {
-        this.hp = hp;
+    public void decHp(int decAmount) {
+        if(decAmount >= hp){
+            hp = 0;
+            isAlive = false;
+        }
+        else{
+            hp = hp - decAmount;
+        }
+    }
+
+    public void incHp(int incAmount){
+        if(incAmount >= 100 - hp){
+            hp = 100;
+        }
+        else{
+            hp = hp + incAmount;
+        }
     }
 
     public int getExp() {
         return exp;
     }
 
-    public void setExp(int exp) {
-        this.exp = exp;
+    public void incExp(int incAmount){
+        exp = exp + incAmount;
+    }
+
+    public void decExp(int decAmount){
+        exp = exp - decAmount;
+    }
+
+    @Override
+    public void moveLeft() {
+        xcoord -= 1;
+    }
+
+    @Override
+    public void moveRight() {
+        xcoord += 1;
+    }
+
+    @Override
+    public void moceUp() {
+        ycoord += 1;
+    }
+
+    @Override
+    public void moveDown() {
+        ycoord -= 1;
     }
 }
 
--- src/test/java/org/openjfx/model/PlayerTest.java
+++ src/test/java/org/openjfx/model/PlayerTest.java
@@ -7,10 +7,51 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    Player player = new Player("1", "Eddy", 0, 0, true, 0, 0 );
+    Player player = new Player("1", 0, 0, true, 100, 0 );
 
     @Test
-    public void playerTest(){
-        Assert.assertEquals(player.isAlive(), true);
+    public void createPlayerTest(){
+        Assert.assertEquals(player.getisAlive(), true);
+        Assert.assertEquals(player.getHp(), 100);
+        Assert.assertEquals(player.getExp(), 0);
+        Assert.assertEquals(player.getId(), "1");
+        Assert.assertEquals(player.getYcoord(), 0);
+        Assert.assertEquals(player.getXcoord(), 0);
+    }
+
+    @Test   //Test of Hp methods
+    public void hpTest(){
+        // Test of decHp when decAmount < Hp
+        player.decHp(50);
+        Assert.assertEquals(player.getHp(), 50);
+
+        player.incHp(30);
+        Assert.assertEquals(player.getHp(), 80);
+
+        player.incHp(500);
+        Assert.assertEquals(player.getHp(), 100);
+
+        player.decHp(200);
+        Assert.assertEquals(player.getHp(), 0);
+        Assert.assertEquals(player.getisAlive(), false);
+    }
+
+    @Test   //Test of Exp methods
+    public void expTest(){
+        player.incExp(200);
+        player.decExp(50);
+
+        Assert.assertEquals(player.getExp(), 150);
+    }
+
+    @Test   //  Test of move functions
+    public void moveTest(){
+        player.moceUp();
+        player.moveDown();
+        player.moveLeft();
+        player.moveRight();
+
+        Assert.assertEquals(player.getXcoord(), 0);
+        Assert.assertEquals(player.getYcoord(), 0);
     }
 }
