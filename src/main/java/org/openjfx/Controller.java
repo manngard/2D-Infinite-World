848ba6106597ff4803d1c72764d5e02febf5f427
Pautrik <patrikemanuelson@gmail.com>
2019-09-19 12:03:40 +0200

    Connected key events from view to controller which invokes methods in model

--- src/main/java/org/openjfx/Controller.java
+++ src/main/java/org/openjfx/Controller.java
@@ -1,28 +1,51 @@
 package org.openjfx;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.openjfx.model.Model;
 import org.openjfx.model.World;
 import org.openjfx.view.View;
 
 import java.io.IOException;
+
+import static javafx.scene.input.KeyCode.*;
 
 /**
  * JavaFX Controller
  */
 public class Controller extends Application {
     View view;
-    World model;
+    Model model;
 
     @Override
     public void start(Stage stage) throws IOException {
         Scene scene = new Scene(new AnchorPane(), 720, 720);
-        view = new View(stage);
-        model = new World();
+        view = new View(stage, this::handleKeyPress);
+        model = new Model();
+    }
+
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch(keyEvent.getCode()) {
+            case UP:
+                model.movePlayerUp();
+                break;
+            case DOWN:
+                model.movePlayerDown();
+                break;
+            case RIGHT:
+                model.movePlayerRight();
+                break;
+            case LEFT:
+                model.movePlayerLeft();
+                break;
+        }
     }
 
     public static void main(String[] args) {
