0773f8a110f03b281166facd95bd867c73a18f1e
johanD98 <dajo9999@hotmail.se>
2019-09-20 17:37:50 +0200

    Actually added Chest as a Child to Entity this time

--- src/main/java/org/openjfx/model/Chest.java
+++ src/main/java/org/openjfx/model/Chest.java
@@ -1,9 +1,13 @@
 package org.openjfx.model;
 
-public class Chest {
+public class Chest extends Entity{
 
-    public Item[] inventory;
-    public Chest(Item[] i){
-        inventory = i;
+    public Item[] inventory = new Item[4];
+    public Chest(String a,int b,int c,Item[] i){
+        super(a,b,c);
+        for(int r = 0;r < 4; r++){
+
+            inventory[r] = i[r];
+        }
     }
 }
--- src/test/java/org/openjfx/model/ChestTest.java
+++ src/test/java/org/openjfx/model/ChestTest.java
@@ -1,4 +1,22 @@
 package org.openjfx.model;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openjfx.model.tilepackage.Tile;
+
 public class ChestTest {
+
+
+    Item[] items = {new Armor("Armor"),new Weapon("Weapon"),new Item("päron"), new Item("karta")};
+    private Chest chest = new Chest("kista", 1, 1,items);
+
+    @Test
+    public void chestest(){
+
+        Assert.assertEquals(chest.inventory[0].getName(),"Armor");
+        Assert.assertEquals(chest.inventory[1].getName(),"Weapon");
+        Assert.assertEquals(chest.inventory[2].getName(),"päron");
+        Assert.assertEquals(chest.inventory[3].getName(),"karta");
+    }
 }
