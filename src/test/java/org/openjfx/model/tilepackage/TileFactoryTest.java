e57f7c196b954e955175f92e094524b3c0539168
Eddy <54838825+EddyIT99@users.noreply.github.com>
2019-09-20 16:26:17 +0200

    Created test class for TileFactory

---
+++ src/test/java/org/openjfx/model/tilepackage/TileFactoryTest.java
@@ -0,0 +1,19 @@
+package org.openjfx.model.tilepackage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileFactoryTest {
+
+
+    @Test
+    public void createTilesTest(){
+        TileFactory factory = new TileFactory();
+        Tile tile = factory.getTile("Lava");
+
+        Assert.assertEquals(tile.);
+    }
+}
+
