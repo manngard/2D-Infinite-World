eca4017fc254d233771d28e2e01a86ce9044c292
pautrik <>
2019-09-21 17:57:38 +0200

    Refactored MVC to match the refactored Event

--- src/main/java/org/openjfx/Controller.java
+++ src/main/java/org/openjfx/Controller.java
@@ -1,23 +1,12 @@
 package org.openjfx;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.openjfx.model.Model;
-import org.openjfx.model.Player;
-import org.openjfx.model.World;
-import org.openjfx.utils.Event;
 import org.openjfx.view.View;
 
 import java.io.IOException;
-
-import static javafx.scene.input.KeyCode.*;
 
 /**
  * JavaFX Controller
@@ -29,7 +18,7 @@ public class Controller extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         model = new Model();
-        view = new View(stage, this::handleKeyPress, model.modelUpdateEvent);
+        view = new View(stage, this::handleKeyPress, model.hasUpdateEvent);
         model.modelHasBeenUpdated();
     }
 
