5d32a6e654fc1f5a97926cf04bef882a19c819e4
manngard <carl.manngard@gmail.com>
2019-09-23 14:28:12 +0200

    Added placeholders for Mountain.png and Lava.png, Created method for getting a Tile with random tiletype

--- src/main/java/org/openjfx/model/tilepackage/TileFactory.java
+++ src/main/java/org/openjfx/model/tilepackage/TileFactory.java
@@ -2,16 +2,20 @@ package org.openjfx.model.tilepackage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TileFactory {
+    List<String> tiletypes = new ArrayList<>();
 
-    public Tile getTile(String id){
-        List<String> tiletypes = new ArrayList<>();
+    public TileFactory(){
         //more tiletypes to be added below
         tiletypes.add("Lava");
         tiletypes.add("Grass");
         tiletypes.add("Mountain");
+    }
 
+
+    public Tile getTile(String id){
         for (String tiletype: tiletypes){
             if (tiletype.equals(id)){
                 return new Tile(id);
@@ -19,5 +23,11 @@ public class TileFactory {
         }
         //Should never occur
         return null;
+    }
+    public Tile getRandomTile(){
+        Random rand = new Random();
+        int randPos = rand.nextInt(tiletypes.size());
+        String id = tiletypes.get(randPos);
+        return new Tile(id);
     }
 }
