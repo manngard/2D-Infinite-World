35b00ee27e7373077bf48a005695cb25208a3760
Eddy <54838825+EddyIT99@users.noreply.github.com>
2019-10-10 12:19:45 +0200

    Made new tests and updated old tests for Chest, Player and World

--- src/test/java/org/openjfx/model/ChestTest.java
+++ src/test/java/org/openjfx/model/ChestTest.java
@@ -7,16 +7,30 @@ import org.openjfx.model.tilepackage.Tile;
 
 public class ChestTest {
 
+    Chest chest = new Chest("Chest1", 0, 0);
 
-    /*Item[] items = {new Armor("Armor"),new Weapon("Weapon"),new Item("päron"), new Item("karta")};
-    private Chest chest = new Chest("kista", 1, 1,items);
 
-    @Test
-    public void chestest(){
+    @Test //    Test to see if the items in chest are random.
+    public void chestRandomTest(){
+        Item item = chest.inventory[1];
 
-        Assert.assertEquals(chest.inventory[0].getName(),"Armor");
-        Assert.assertEquals(chest.inventory[1].getName(),"Weapon");
-        Assert.assertEquals(chest.inventory[2].getName(),"päron");
-        Assert.assertEquals(chest.inventory[3].getName(),"karta");
-    }*/
+        if(chest.inventory[0].getName().equals("Axe")){
+            System.out.println("Item is: Axe at [0]");
+            Assert.assertEquals(chest.inventory[0].getName(),"Axe");
+        }else if(chest.inventory[1].getName().equals("Sword")){
+            System.out.println("Item is: Sword at [1]");
+            Assert.assertEquals(chest.inventory[1].getName(),"Sword");
+
+        }else if(chest.inventory[2].getName().equals("Plate armor")){
+            System.out.println("Item is: Plate armor at [2]");
+            Assert.assertEquals(chest.inventory[2].getName(),"Plate armor");
+
+        }else if(chest.inventory[3].getName().equals("Cloth armor")){
+            System.out.println("Item is: Cloth armor at [3]");
+            Assert.assertEquals(chest.inventory[3].getName(),"Cloth armor");
+
+        }
+
+
+    }
 }
