0c17d0db7e0270d5b4e9306f41a1d01ee4962d7e
Eddy <54838825+EddyIT99@users.noreply.github.com>
2019-10-22 14:45:22 +0200

    Added more world tests

--- src/test/java/org/openjfx/model/WorldTest.java
+++ src/test/java/org/openjfx/model/WorldTest.java
@@ -1,9 +1,11 @@
 package org.openjfx.model;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openjfx.model.tile.Tile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -23,6 +25,27 @@ public class WorldTest {
             columns++;
         }
         assertTrue(columns == world.worldHorizontalSideLength);
+    }
+
+    //  Test to see if the attackHit method in world class works. This also ensures that the method inSight works.
+    @Test
+    public void worldCombatTest(){
+        List<Combatant> enemies = new ArrayList<>();
+        Enemy enemy = new Enemy("e1", -1, 0, 100, 20, 2, 0);
+        enemies.add(enemy);
+        Assert.assertTrue(world.inSight(world.player, enemy));
+        world.attackHit(world.player, world.combatantAttacks(world.player, enemies));
+        Assert.assertEquals(80, enemy.getHp());
+
+        Enemy enemy2 = new Enemy("e2", 100, 100, 100, 20, 2, 0);
+        enemies.add(enemy2);
+
+        Assert.assertFalse(world.inSight(world.player, enemy2));
+
+        world.combatantAttacks(world.player, enemies);
+        world.attackHit(world.player, world.combatantAttacks(world.player, enemies));
+        Assert.assertEquals(60, enemy.getHp());
+        Assert.assertEquals(100, enemy2.getHp());
     }
 
 
