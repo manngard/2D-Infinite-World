76e5882c7924ae919bde0634edea15c7a129a82b
Eddy <54838825+EddyIT99@users.noreply.github.com>
2019-10-25 19:59:26 +0200

    Tried to make a createRandomTiles test but cannot be done since OpenSimplexAdapter's constructor is private.

--- src/test/java/org/openjfx/model/entity/tile/TileFactoryTest.java
+++ src/test/java/org/openjfx/model/entity/tile/TileFactoryTest.java
@@ -1,17 +1,50 @@
 package org.openjfx.model.entity.tile;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openjfx.model.noise.DefaultNoiseGenerator;
+import org.openjfx.model.noise.NoiseGenerator;
 
-class TileFactoryTest {
+import static org.junit.Assert.assertEquals;
 
-/*
+public class TileFactoryTest {
+
+
+    // Not valid test, but creating random tiles works perfectly in real application.
     @Test
     public void createTilesTest(){
-        TileFactory factory = new TileFactory();
-        Tile tile = factory.getTile("Lava",10,10);
 
-        Assert.assertEquals("Lava", tile.getId());
+        TileFactory factory = new TileFactory(new DefaultNoiseGenerator()); //Test cannot be done since OpenSimplexAdapter's is private.
+        Tile tile = factory.generateTile(10,10);
+        int lavaCounter = 0;
+        int grassCounter = 0;
+        int mountainCounter = 0;
 
-    }*/
+
+        for(int i = 0; i < 100; i++){
+            factory.generateTile(i,i);
+            if(tile.getId().equals("Lava")){
+                lavaCounter ++;
+            }
+            else if(tile.getId().equals("Grass")){
+                grassCounter ++;
+            }
+            else if(tile.getId().equals("Mountain")){
+                mountainCounter ++;
+            }
+        }
+        Tile tileIsRandom = factory.generateTile(101,101);
+
+        assertEquals(tileIsRandom.getId(), tileIsRandom.getId());
+
+        System.out.println(
+                "Amount of lava tiles created: " + lavaCounter + "\n" +
+                "Amount of grass tiles created: " + grassCounter + "\n" +
+                "Amount of mountain tiles created: " + mountainCounter
+        );
+
+    //  This test is not valid!
+
+    }
 }
 
